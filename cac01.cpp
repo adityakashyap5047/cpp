e9485b8d3279082edcfc39309b0991d41f6d77a6
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-08-31 03:21:11 +0530

    Adding the initial code of CPP

---
+++ cac01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello Chai from Aditya";
+
+        // To run this code in terminal, use the following commands
+        // g++ cac01.cpp -o cac01
+        // ./cac01
+        
+    return 0;
+}
