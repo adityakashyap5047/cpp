564f7ce71497a2cde5da4ea5a3194416b885e87f
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-08-31 03:25:36 +0530

    Removing the unnecessary code

--- cac01.cpp
+++ cac01.cpp
@@ -5,10 +5,5 @@ using namespace std;
 int main()
 {
     cout << "Hello Chai from Aditya";
-
-        // To run this code in terminal, use the following commands
-        // g++ cac01.cpp -o cac01
-        // ./cac01
-        
     return 0;
 }
