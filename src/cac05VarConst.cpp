a716f9602557c7e94b1c95a74a810a60489167d2
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-05-22 13:19:46 +0530

    comments in cpp

---
+++ src/cac05VarConst.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Welcome to CPP" << endl;
+
+    // This is a comment
+
+    /*
+        This is a multi-line comment
+        It can span multiple lines
+    */
+
+    return 0;
+}
