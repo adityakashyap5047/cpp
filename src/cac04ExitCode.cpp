913a6570c86e6ff4f91cdd0de80cd415856ef7f9
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-05-22 12:48:57 +0530

    exit code in cpp

---
+++ src/cac04ExitCode.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello cpp from Aditya" << endl;
+    
+    return 4;   // Exit code which indicates what task should we done for which exit code
+    // 0 - Success
+    // 1 - Failure
+    // 2 - Warning
+    // 3 - Error
+    // 4 - Information
+}
