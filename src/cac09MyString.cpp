5d329d4c08c594b6406f1c8cf44abae2e40d23a6
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-05-24 07:52:12 +0530

    string in cpp with escape sequence character

---
+++ src/cac09MyString.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string name = "Lemon Tea \n";
+    string description = "A refreshing \"blend\" of lemon and tea.";
+
+    cout << "Tea name: " << name ;
+    cout << "Description: " << description << endl;
+
+    return 0;
+}
