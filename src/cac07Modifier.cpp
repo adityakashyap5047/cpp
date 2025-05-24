731339605aac956108e62669e6cc39fab0b13def
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-05-24 07:40:02 +0530

    modifier in cpp

---
+++ src/cac07Modifier.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    unsigned smallTeaPack = 1200;
+
+    long largeTeaPack = 1200000;
+
+    cout << largeTeaPack << endl;
+
+    return 0;
+}
